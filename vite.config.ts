f7d2357c20192b7faf48c48b5613241b6181327b
okayshahi <shahidahmohd@gmail.com>
2022-06-05 11:32:35 +0800

    installable app

--- vite.config.ts
+++ vite.config.ts
@@ -9,34 +9,17 @@ export default defineConfig({
   },
   plugins: [
     VitePWA({
-      // you can remove base and scope pwa plugin will use the base on vite: defaults to /
-      base: "/",
-      scope: "/",
-      registerType: "autoUpdate",
-      injectRegister: false,
-      manifest: false,
-      workbox: {
+      strategies: "injectManifest",
+      injectManifest: {
+        swSrc: 'public/sw.js',
+        swDest: 'dist/sw.js',
         globDirectory: 'dist',
         globPatterns: [
-          '**/*.{html,js,css,png,webp,jpg}'
+          '**/*.{html,js,css,svg,png,webp,jpg}',
         ],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/unpkg\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'unpkg-libs-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
-              },
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          }
-        ]
       },
+      registerType: 'autoUpdate',
+      manifest: false,
       devOptions: {
         enabled: true
       }
